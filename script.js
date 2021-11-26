7b677017f17f8aff26c0d44619884fe5f847f44a
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-26 10:36:09 -0300

    Adicionado o requisito 18

--- script.js
+++ script.js
@@ -1,44 +1,80 @@
 const button = document.getElementById('criar-carta');
 const imput = document.getElementById('carta-texto');
 const father = document.getElementById('carta-gerada');
+const contador = document.getElementById('carta-contador');
 
 const grupoEstilo = ['newspaper', 'magazine1', 'magazine2'];
 const grupoTamanho = ['medium', 'big', 'reallybig'];
 const grupoRotacao = ['rotateleft', 'rotateright'];
 const grupoInclinacao = ['skewleft', 'skewright'];
 
+let classesEscolhidas = [];
+let classesMisturadas = [];
 let palavras = [];
+let palavra;
 
-function creatCarta() {
+let dadosEstilo;
+let dadosTamanho;
+let dadosRotacao;
+let dadosInclinacao;
+let dadosOrdenClasse; // valor entre 0 e 3
+let dadosQuantidadeClasse;
+
+
+function embaralharClassesEscolhidas(){
+   
+
+
+}
+
+function putClass() {
+  classesEscolhidas = [];
+  classesEscolhidas.push(grupoEstilo[dadosEstilo]);
+  classesEscolhidas.push(grupoTamanho[dadosTamanho]);
+  classesEscolhidas.push(grupoRotacao[dadosRotacao]);
+  classesEscolhidas.push(grupoInclinacao[dadosInclinacao]);
+
+  embaralharClassesEscolhidas();
+
+  console.log(classesEscolhidas);
+
+  for (i = 0; i <= dadosQuantidadeClasse; i += 1) { // dadosQuantidadeClasse entre 0 e 3
+    palavra.classList.add(classesEscolhidas[i]);
+  }
+  father.appendChild(palavra);
+  console.log(palavra.className);
+}
+
+function jogarDados() {
   for (let i = 0; i < palavras.length; i += 1) {
-    const dadosEstilo = Math.trunc(Math.random() * 4);
-    const dadosTamanho = Math.trunc(Math.random() * 4);
-    const dadosRotacao = Math.trunc(Math.random() * 2);
-    const dadosInclinacao = Math.trunc(Math.random() * 2);
+    dadosEstilo = Math.trunc(Math.random() * 3);
+    dadosTamanho = Math.trunc(Math.random() * 3);
+    dadosRotacao = Math.trunc(Math.random() * 2);
+    dadosInclinacao = Math.trunc(Math.random() * 2);
+    dadosQuantidadeClasse = Math.trunc(Math.random() * 4);
+    dadosOrdenClasse = Math.trunc(Math.random() * 4);
 
-    const palavra = document.createElement('span');
+    palavra = document.createElement('span');
     palavra.innerText = palavras[i];
 
-    palavra.classList.add(grupoEstilo[dadosEstilo]);
-    palavra.classList.add(grupoTamanho[dadosTamanho]);
-    palavra.classList.add(grupoRotacao[dadosRotacao]);
-    palavra.classList.add(grupoInclinacao[dadosInclinacao]);
-    father.appendChild(palavra);
+    putClass();
   }
 }
 
 function inputRead() {
   palavras = imput.value.split(' ');
 
-  let stringVazia = imput.value.replace(/\s/g, '');
+  const stringVazia = imput.value.replace(/\s/g, '');
 
   if (imput.value.length === 0 || stringVazia.length === 0) {
-    alert('Por favor, digite o conteúdo da carta.');
+    const alerta = document.createElement('span');
+    alerta.innerText = 'Por favor, digite o conteúdo da carta.';
+    alerta.id = 'carta-gerada';
+    father.appendChild(alerta);
   } else {
-    creatCarta();
+    contador.innerText = palavras.length
+    jogarDados();
   }
 }
 
 button.addEventListener('click', inputRead);
-
-
